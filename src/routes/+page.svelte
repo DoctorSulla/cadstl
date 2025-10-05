bf61fad00419a6072cd0387205d119ab7f9b67ba
Matthew Halliday <matthew.halliday@gmail.com>
2025-10-05 08:28:44 +0100

    Changed landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="m-auto text-center text-xl">
+	Welcome to the new home for the Chester and District Summer Tennis League
+</h1>
+
+<div class="m-auto my-4 w-fit rounded-2xl bg-red-400 p-4 text-red-900">
+	This website is currently under construction.
+</div>
