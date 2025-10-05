a37aed86edbf0077bdfc3a188972ac1ed46b8675
DoctorSulla <matthew.halliday@gmail.com>
2025-10-05 18:15:05 +0100

    Added photo carousel

---
+++ src/lib/PhotoCarousel.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	let { photos = [] } = $props();
+
+	let currentPhoto = $state(0);
+	let numberOfPhotos = $derived(photos.length);
+
+	function increment() {
+		if (currentPhoto + 1 < numberOfPhotos) {
+			console.log(currentPhoto);
+			currentPhoto++;
+		} else {
+			currentPhoto = 0;
+		}
+	}
+
+	function decrement() {
+		if (currentPhoto > 0) {
+			currentPhoto--;
+		} else {
+			currentPhoto = numberOfPhotos - 1;
+		}
+	}
+</script>
+
+<div class="m-auto text-center">
+	<img class="m-auto" alt={photos[currentPhoto].caption} src={photos[currentPhoto].src} />
+	<p class="mb-3 text-center">
+		{photos[currentPhoto].caption}
+	</p>
+
+	<div class="flex justify-center align-middle text-xl">
+		<button class="mr-2 cursor-pointer text-blue-500" type="button" onclick={decrement}
+			><i class="fa-solid fa-arrow-left"></i> Previous</button
+		>
+		<span>Photo {currentPhoto + 1} of {numberOfPhotos}</span>
+		<button class="ml-2 cursor-pointer text-blue-500" type="button" onclick={increment}
+			>Next <i class="fa-solid fa-arrow-right"></i></button
+		>
+	</div>
+</div>
---
+++ src/routes/photos/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import PhotoCarousel from '$lib/PhotoCarousel.svelte';
+
+	let photos = [
+		{ src: '/photos/2024/ladiesd1.avif', caption: "Ladies' division 1 - Wrexham" },
+		{ src: '/photos/2024/ladiesd2.avif', caption: "Ladies' division 2 - Chester A" },
+		{ src: '/photos/2024/ladiesd3.avif', caption: "Ladies' division 3 - Malpas" },
+		{ src: '/photos/2024/mensd3.avif', caption: "Men's division 3 - Cheshire Oaks" },
+		{ src: '/photos/2024/mixedd2.avif', caption: 'Mixed division 2 - Cheshire Oaks A' },
+		{ src: '/photos/2024/mixedd3.avif', caption: 'Mixed division 3 - Hooton' },
+		{ src: '/photos/2024/vets.avif', caption: "Veteran's division - Heswall" }
+	];
+</script>
+
+<div class="mt-2">
+	<PhotoCarousel {photos} />
+</div>
