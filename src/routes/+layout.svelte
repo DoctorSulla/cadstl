586215707152fbf6d566061708dea7d4c41a5eac
Matthew Halliday <matthew.halliday@gmail.com>
2025-10-05 10:58:52 +0100

    Added some routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
+
+	const routes = {
+		'/': 'Home',
+		'/history': 'History',
+		'/rules': 'Rules',
+		'/photos': 'Photos',
+		'/results': 'Results',
+		'/committee': 'Committee',
+		'/scorecards': 'Scorecards'
+	};
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+{#each Object.entries(routes) as [key, value]}
+	<a class="mx-1 hover:underline" href={key}>{value}</a>
+{/each}
 
 {@render children?.()}
