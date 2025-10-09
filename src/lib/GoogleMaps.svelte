67ae3f420d5bab867026ba7dbcaa75595157dc0e
DoctorSulla <matthew.halliday@gmail.com>
2025-10-09 17:22:37 +0100

    Added registration form and fixed bug which caused maps API to load on route refresh

--- src/lib/GoogleMaps.svelte
+++ src/lib/GoogleMaps.svelte
@@ -1,5 +1,11 @@
 <script context="module" lang="ts">
 	declare const google: any;
+</script>
+
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let googleMapsWrapper: HTMLElement;
 
 	const locations = [
 		{ title: 'Chester Tennis Club', position: { lat: 53.1757, lng: -2.8956 } },
@@ -14,49 +20,44 @@
 		{ title: 'Mold Tennis Club', position: { lat: 53.1674, lng: -3.14576 } },
 		{ title: 'Hollies Tennis Club', position: { lat: 52.9735, lng: -2.6892 } },
 		{ title: 'Brymbo Tennis Club', position: { lat: 53.0642, lng: -3.0473 } },
-		{ title: 'Hoylake Tennis Club', position: { lat: 53.3826, lng: -3.1843 } }
+		{ title: 'Hoylake Tennis Club', position: { lat: 53.3826, lng: -3.1843 } },
+		{ title: 'Tattenhall Tennis Club', position: { lat: 53.1249, lng: -2.7653 } },
+		{ title: 'Neston Tennis Club', position: { lat: 53.2969, lng: -3.0757 } }
 	];
-</script>
 
-<script lang="ts">
-	import { onMount } from 'svelte';
+	function generateCadstlMap() {
+		const map = new google.maps.Map(googleMapsWrapper, {
+			mapId: '7cb7e0c11d7a014c439a8126',
+			center: { lat: 53.1934, lng: -2.8943 },
+			zoom: 9
+		});
 
-	let googleMapsWrapper: HTMLElement;
+		for (let location of locations) {
+			new google.maps.marker.AdvancedMarkerElement({
+				map,
+				position: location.position,
+				title: location.title
+			});
+		}
+	}
 
 	onMount(async () => {
-		const script = document.createElement('script');
-		script.src =
-			'https://maps.googleapis.com/maps/api/js?key=AIzaSyBkoWsxb0Jk1YjDvhz0v9zLevAADKEkZU8&libraries=maps,marker';
-		script.async = true;
-		script.defer = true;
-		script.onload = () => {
-			const map = new google.maps.Map(googleMapsWrapper, {
-				mapId: '7cb7e0c11d7a014c439a8126',
-				center: { lat: 53.1934, lng: -2.8943 },
-				zoom: 9
-			});
+		let check = document.getElementById('google-maps-script');
+		if (!check) {
+			const script = document.createElement('script');
+			script.id = 'google-maps-script';
+			script.src =
+				'https://maps.googleapis.com/maps/api/js?key=AIzaSyBkoWsxb0Jk1YjDvhz0v9zLevAADKEkZU8&libraries=maps,marker';
+			script.async = true;
+			script.defer = true;
 
-			for (let location of locations) {
-				/*let dot = document.createElement('div');
-				dot.style.width = '15px';
-				dot.style.height = '15px';
-				dot.style.backgroundColor = 'red';
-				dot.style.borderRadius = '100%';
-				dot.style.border = '1px solid black';
-				let label = document.createElement('div');
-				label.style.textAlign = 'center';
-				label.style.fontWeight = 'bold';
-				label.appendChild(dot);
-				label.innerHTML += location.title; */
-
-				new google.maps.marker.AdvancedMarkerElement({
-					map,
-					position: location.position,
-					title: location.title
-				});
-			}
-		};
-		document.head.appendChild(script);
+			script.onload = () => {
+				generateCadstlMap();
+			};
+			document.head.appendChild(script);
+		} else {
+			generateCadstlMap();
+		}
 	});
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,24 @@
 	Chester and District Summer Tennis League
 </h1>
 
-<p class="mt-2 text-2xl">
-	Welcome to the CADSTL website with all of the latest information concerning the 2025 season,
-	including:
+<p class="my-5 text-2xl">
+	Welcome to the CADSTL website. As the name would suggest, we are a Chester based tennis league but
+	have teams across the Wirral as well as a few in North Wales. If you are interested in joining the
+	league please fill out a <a
+		class="text-blue-700 underline hover:text-blue-900"
+		href="/cadstl_registration_form_2025.doc">registration form <i class="fas fa-file-word"></i></a
+	>.
 </p>
-<ul class="my-2 list-disc">
-	<li class="ml-4">2025 fixtures and teams</li>
-	<li class="ml-4">The latest league tables</li>
-	<li class="ml-4">The league rules</li>
-	<li class="ml-4">AGM minutes</li>
-	<li class="ml-4">Committee contact details</li>
+
+<p class="my-5 text-2xl">
+	This website has all the latest information concerning the 2025 season, including:
+</p>
+<ul class="mt-2 mb-10 list-disc text-xl">
+	<li class="ml-4"><i class="fa-solid fa-calendar-days"></i> 2025 fixtures and teams</li>
+	<li class="ml-4"><i class="fa-solid fa-medal"></i> The latest league tables</li>
+	<li class="ml-4"><i class="fa-solid fa-scale-balanced"></i> The league rules</li>
+	<li class="ml-4"><i class="fa-solid fa-memo"></i> AGM minutes</li>
+	<li class="ml-4"><i class="fa-solid fa-address-card"></i> Committee contact details</li>
 </ul>
 
 <GoogleMaps />
